aed4d4e5bb84cd67e261c04fbacabfe9797f948a
J-P Nurmi <jpnurmi@gmail.com>
2022-03-20 20:09:10 +0100

    Add Errno exception

--- lib/src/errno.dart
+++ lib/src/errno.dart
@@ -1,4 +1,27 @@
+import 'package:meta/meta.dart';
+
 import 'libc.dart';
+import 'string.dart';
 
 int get errno => libc.errno;
 set errno(int errno) => libc.errno = errno;
+
+@immutable
+class Errno implements Exception {
+  Errno(this.id, this.errno);
+
+  final int errno;
+  final String id;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is Errno && other.errno == errno && other.id == id;
+  }
+
+  @override
+  int get hashCode => Object.hash(errno, id);
+
+  @override
+  String toString() => 'Errno($errno, $id: "${strerror(errno)}")';
+}
--- lib/src/util.dart
+++ lib/src/util.dart
@@ -2,6 +2,14 @@ import 'dart:ffi' as ffi;
 
 import 'package:ffi/ffi.dart' as ffi;
 
+import 'errno.dart';
+
+void checkErrno(String id, int res) {
+  if (res < 0) {
+    throw Errno(id, errno);
+  }
+}
+
 extension Int8PointerString on ffi.Pointer<ffi.Int8> {
   String? toDartString({int? length}) {
     if (this == ffi.nullptr) return null;
