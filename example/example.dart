47e9cfb0e78a7725a8594196caf1eead66f4f09e
J-P Nurmi <jp.nurmi@canonical.com>
2022-06-25 21:43:53 +0200

    Add example

---
+++ example/example.dart
@@ -0,0 +1,15 @@
+import 'package:stdlibc/stdlibc.dart';
+
+void main() {
+  if (geteuid() == 0) {
+    print('WARNING: running as root');
+  }
+
+  final utsname = uname();
+  print('System: ${utsname.sysname} (${utsname.nodename})');
+  print('Version: ${utsname.release} (${utsname.machine})');
+
+  final info = sysinfo();
+  print('Uptime: ${info.uptime}');
+  print('Load average: ${info.loads[0] / (1 << SI_LOAD_SHIFT)}');
+}
