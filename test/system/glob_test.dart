6f904ab7b87a6df80ae18b68ad2fd4acf5896c04
J-P Nurmi <jp.nurmi@canonical.com>
2022-08-14 20:28:59 +0200

    Move existing tests to test/system/

--- test/system/glob_test.dart
+++ test/system/glob_test.dart
@@ -3,8 +3,9 @@ import 'package:test/test.dart';
 
 void main() {
   test('glob', () {
-    expect(glob('test/glob_*.dart'), equals(['test/glob_test.dart']));
-    expect(glob('test/*_test.dart').length, greaterThan(1));
+    expect(glob('test/system/glob_*.dart'),
+        equals(['test/system/glob_test.dart']));
+    expect(glob('test/system/*_test.dart').length, greaterThan(1));
     expect(glob('~', flags: GLOB_TILDE), isNotEmpty);
     expect(() => glob('nothing'), throwsA(isA<GlobException>()));
   });
