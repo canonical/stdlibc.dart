ba88f47f31aaa1578c879d85d908863db7f5c11a
J-P Nurmi <jp.nurmi@canonical.com>
2022-08-14 07:54:15 +0200

    Add ioctl() overloads for 3rd int/ptr args

--- lib/src/ioctl.dart
+++ lib/src/ioctl.dart
@@ -1,7 +1,7 @@
 import 'libc.dart';
 
-int ioctl(int fd, int request) => libc.ioctl(fd, request);
+int ioctl(int fd, int request, [dynamic arg]) => libc.ioctl(fd, request, arg);
 
 mixin IoctlMixin {
-  int ioctl(int fd, int request);
+  int ioctl(int fd, int request, dynamic arg);
 }
