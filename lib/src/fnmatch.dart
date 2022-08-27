f4bfd81b7e44bc3bb56acad0150531d65cec19dd
J-P Nurmi <jp.nurmi@canonical.com>
2022-08-27 19:17:45 +0200

    fnmatch(): remove internal errno check

--- lib/src/fnmatch.dart
+++ lib/src/fnmatch.dart
@@ -1,13 +1,7 @@
-import 'macros.g.dart';
 import 'platform.dart';
-import 'util.dart';
 
-bool fnmatch(String pattern, String name, {int flags = 0}) {
-  final res = platform.fnmatch(pattern, name, flags);
-  if (res != 0 && res != FNM_NOMATCH) {
-    checkErrno('fnmatch', res);
-  }
-  return res == 0;
+int fnmatch(String pattern, String name, {int flags = 0}) {
+  return platform.fnmatch(pattern, name, flags);
 }
 
 mixin FnmatchMixin {
