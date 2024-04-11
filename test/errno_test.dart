0bb56b1ded331aa97d4c0e6844700de64b15e485
Robert Ancell <robert.ancell@canonical.com>
2024-04-12 11:05:28 +1200

    Disable default errno value test
    On Ubuntu 20.04 this was returning EINVAL, from some code outside of this test

--- test/errno_test.dart
+++ test/errno_test.dart
@@ -3,7 +3,8 @@ import 'package:test/test.dart';
 
 void main() {
   test('errno', () {
-    expect(errno, isZero);
+    // Disabled as this was returning EINVAL in Ubuntu 20.04, from some other code
+    //expect(errno, isZero);
 
     errno = 123;
     expect(errno, equals(123));
