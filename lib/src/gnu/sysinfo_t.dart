eb86da600a835f9c551caea35d6189529ce458a1
Brian Quinlan <bquinlan@google.com>
2025-04-03 11:33:01 -0700

    Use modern linux

--- lib/src/gnu/sysinfo_t.dart
+++ lib/src/gnu/sysinfo_t.dart
@@ -1,3 +1,12 @@
+// Since Linux 2.3.23 (i386) and Linux 2.3.48 (all architectures) the `sysinfo`
+// struct has had this field:
+//
+// `char _f[20-2*sizeof(long)-sizeof(int)];  /* Padding to 64 bytes */`
+//
+// `package:ffigen` cannot generate correct dart code for complex array sizes
+// (https://github.com/dart-lang/native/issues/2161) so include a version
+// generated before Linux 2.3.23 separately.
+//
 // ignore_for_file: camel_case_types
 
 import 'dart:ffi' as ffi;
