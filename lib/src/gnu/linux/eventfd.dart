648e73494a57bf968faf4899d33a9919c644e89e
Hannes Winkler <hanneswinkler2000@web.de>
2025-05-02 20:42:39 +0200

    manually check initial value of eventfd is 32-bits

--- lib/src/gnu/linux/eventfd.dart
+++ lib/src/gnu/linux/eventfd.dart
@@ -9,6 +9,19 @@ mixin GnuLinuxEventFdMixin on PlatformLibC {
     bool nonblock = false,
     bool semaphore = false,
   }) {
+    // The value used to initialize an eventfd is a uint32_t, while
+    // when reading and writing to it, it's basically an uint64_t.
+    //
+    // So users might pass something larger than 32-bits here, assuming it will
+    // work, but in fact dart ffi semantics will truncate it to 32-bits.
+    RangeError.checkValueInInterval(
+      initialValue,
+      0,
+      UINT32_MAX,
+      'initialValue',
+      'initialValue must be between 0 and $UINT32_MAX',
+    );
+
     var flags = 0;
     if (cloexec) {
       flags |= ffi.EFD_CLOEXEC;
