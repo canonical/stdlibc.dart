9a0ef51f593ead68519d5e7302674195d67bef8c
Hannes Winkler <hanneswinkler2000@web.de>
2025-05-02 18:18:42 +0200

    add support for eventfd & epoll

---
+++ lib/src/gnu/linux/epoll.dart
@@ -0,0 +1,110 @@
+import 'dart:collection';
+import 'dart:ffi' as ffi;
+
+import 'package:ffi/ffi.dart' as ffi;
+
+import '../../platform.dart';
+import '../ffigen.dart' as ffi;
+import '../ffigen.dart';
+import '../gnu.dart';
+import 'epoll_buffer.dart';
+
+/// The operation that epoll_ctl should perform.
+///
+/// See also: https://man7.org/linux/man-pages/man2/epoll_ctl.2.html
+enum EpollOp {
+  add(ffi.EPOLL_CTL_ADD),
+  mod(ffi.EPOLL_CTL_MOD),
+  del(ffi.EPOLL_CTL_DEL);
+
+  const EpollOp(this.native);
+
+  final int native;
+}
+
+typedef EpollEvent = ({int event, int metadata});
+
+mixin GnuLinuxEpollMixin on PlatformLibC {
+  /// Creates a new epoll instance.
+  ///
+  /// See also: https://man7.org/linux/man-pages/man2/epoll_create.2.html
+  int epoll_create({bool cloexec = true}) {
+    var flags = 0;
+    if (cloexec) {
+      flags |= ffi.EPOLL_CLOEXEC;
+    }
+    return gnu.epoll_create1(flags);
+  }
+
+  /// Add, modify, or remove entries in the interest list of the epoll
+  /// instance referred to by [epollFd].
+  ///
+  /// See also: https://man7.org/linux/man-pages/man2/epoll_ctl.2.html
+  int epoll_ctl(int epollFd, EpollOp op, int fd, int eventFlags, int metadata) {
+    return ffi.using((arena) {
+      final event = arena<epoll_event>();
+
+      event.ref.events = eventFlags;
+      event.ref.data.u64 = metadata;
+
+      return gnu.epoll_ctl(epollFd, op.native, fd, event);
+    });
+  }
+
+  /// Wait for I/O events on an epoll file descriptor.
+  ///
+  /// [epollFd] is an epoll file descriptor returned by epoll_create.
+  /// [timeout] is the maximum time to wait for events. A [null] value here
+  /// means to block indefinitely, while a [Duration.zero] means to return
+  /// immediately.
+  ///
+  /// If [into] is provided, it will be used to store the events, rather
+  /// than creating a new list and copying the events into it.
+  ///
+  /// If [maxEvents] is provided, it will be used to limit the number of
+  /// events returned.
+  ///
+  /// Returns `null` on error.
+  ///
+  /// See: https://man7.org/linux/man-pages/man2/epoll_wait.2.html
+  List<EpollEvent>? epoll_wait(
+    int epollFd, {
+    Duration? timeout = Duration.zero,
+    EpollBuffer? into,
+    int? maxEvents,
+  }) {
+    return ffi.using((arena) {
+      final memory = into ??
+          EpollBuffer.allocate(
+            maxEvents ?? 10,
+            allocator: arena,
+          );
+
+      try {
+        memory.useRaw((ptr, maxEvents) {
+          final result = gnu.epoll_wait(
+            epollFd,
+            ptr,
+            maxEvents,
+            timeout?.inMilliseconds ?? -1,
+          );
+          if (result < 0) {
+            throw Exception('epoll_wait failed');
+          } else {
+            return result;
+          }
+        });
+      } on Exception {
+        return null;
+      }
+
+      if (into == null) {
+        // Since we're free-ing the buffer again, copy the data to a new
+        // (gc-ed) List.
+        return List.of(memory);
+      } else {
+        return memory;
+      }
+    });
+  }
+}
---
+++ lib/src/linux/epoll.dart
@@ -0,0 +1,66 @@
+import '../gnu/linux/epoll.dart';
+import '../gnu/linux/epoll_buffer.dart';
+import '../platform.dart';
+
+/// Creates a new epoll instance.
+///
+/// See also: https://man7.org/linux/man-pages/man2/epoll_create.2.html
+int epoll_create({bool cloexec = true}) {
+  if (platform is! GnuLinuxEpollMixin) {
+    throw UnsupportedError('epoll_create is only supported on Linux');
+  }
+
+  return (platform as GnuLinuxEpollMixin).epoll_create(cloexec: cloexec);
+}
+
+/// Add, modify, or remove entries in the interest list of the epoll
+/// instance referred to by [epollFd].
+///
+/// See also: https://man7.org/linux/man-pages/man2/epoll_ctl.2.html
+int epoll_ctl(int epollFd, EpollOp op, int fd, int eventFlags, int metadata) {
+  if (platform is! GnuLinuxEpollMixin) {
+    throw UnsupportedError('epoll_create is only supported on Linux');
+  }
+
+  return (platform as GnuLinuxEpollMixin).epoll_ctl(
+    epollFd,
+    op,
+    fd,
+    eventFlags,
+    metadata,
+  );
+}
+
+/// Wait for I/O events on an epoll file descriptor.
+///
+/// [epollFd] is an epoll file descriptor returned by epoll_create.
+/// [timeout] is the maximum time to wait for events. A [null] value here
+/// means to block indefinitely, while a [Duration.zero] means to return
+/// immediately.
+///
+/// If [into] is provided, it will be used to store the events, rather
+/// than creating a new list and copying the events into it.
+///
+/// If [maxEvents] is provided, it will be used to limit the number of
+/// events returned.
+///
+/// Returns `null` on error.
+///
+/// See: https://man7.org/linux/man-pages/man2/epoll_wait.2.html
+List<EpollEvent>? epoll_wait(
+  int epollFd, {
+  Duration? timeout = Duration.zero,
+  EpollBuffer? into,
+  int? maxEvents,
+}) {
+  if (platform is! GnuLinuxEpollMixin) {
+    throw UnsupportedError('epoll_create is only supported on Linux');
+  }
+
+  return (platform as GnuLinuxEpollMixin).epoll_wait(
+    epollFd,
+    timeout: timeout,
+    into: into,
+    maxEvents: maxEvents,
+  );
+}
